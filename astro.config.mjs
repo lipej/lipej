05b1674b557b688febb058ce37e536e02a1eda09
Felipe José <dasilvalipejose@gmail.com>
2024-10-11 12:00:26 -0300

    fix: change to root domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://blog.lipej.dev",
+  site: "https://lipej.dev",
   prefetch: {
     prefetchAll: true,
   },
