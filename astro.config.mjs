fc38288ff80ed542e6c2eba32c6b4faac839da63
Felipe José <dasilvalipejose@gmail.com>
2024-10-11 11:30:40 -0300

    fix: base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://lipej.github.io",
-  base: "/nblog",
+  base: "/",
   prefetch: {
     prefetchAll: true,
   },
